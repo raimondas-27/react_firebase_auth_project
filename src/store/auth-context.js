f27084eb0d026ecdec38d8e11d86b8e1d995239b
raimondas-27 <raimondasvilnius@yahoo.com>
2021-08-25 11:33:02 +0300

    auth context and auth-context provider functionality

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,5 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 
-const AuthContext = React.createContext();
+const AuthContext = React.createContext({
+   token: "",
+   isLoggedIn: false,
+   login: (token) => {
+   },
+   logout: () => {
+   },
+});
+
+export const AuthContextProvider = (props) => {
+
+   const [token, setToken] = useState("");
+   const isLoggedIn = !!token;
+
+   const loginHandler = (token) => {
+      setToken(token);
+   }
+
+   const logoutHandler = () => {
+      setToken(null);
+   }
+
+   const contextValue = {
+      token: token,
+      isLoggedIn: isLoggedIn,
+      login: loginHandler,
+      logout: logoutHandler,
+   }
+
+   return (
+       <AuthContext.Provider value={contextValue}>
+          {props.children}
+       </AuthContext.Provider>
+   )
+}
 
 export default AuthContext;
